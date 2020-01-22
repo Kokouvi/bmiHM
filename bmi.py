cfa90017cbb211e840e4a7ddc095b7b10131a282
Tossenou <kokouvi.tossenou@sap.com>
2020-01-22 09:09:04 +0000

    My BMI

---
+++ bmi.py
@@ -0,0 +1,11 @@
+# Program showing how to calculate BMI
+
+weight = 65
+
+## Need the value in height suared
+
+height = (1.80**2) 
+
+BMI = round(weight / height, 2)
+
+print("Your Body Weight is", BMI, "kg/msquared", "(Normal)")
